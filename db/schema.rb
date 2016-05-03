6a5ffec64427be4e92f5bce77033ec2b584bf9e9
jayc0b0 <jaorner@gmail.com>
2016-05-02 20:02:19 -0700

    Finalized book database schema, FOR NOW

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429232724) do
+ActiveRecord::Schema.define(version: 20160503025930) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20160429232724) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "filename"
-    t.string   "path"
     t.string   "publisher"
     t.string   "extension"
     t.string   "shahash"
